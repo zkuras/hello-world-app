3da359e772ae0cd35748f4be1bef1a0c0981e1c0
Zkuras <zkuras@yahoo.com>
2021-06-10 23:55:35 +0000

    added files

---
+++ deployments/terraform/hello-world.tfvars
@@ -0,0 +1,5 @@
+google_project_id       = "peppy-house-314119"
+google_bucket_name      = "zhanara11"
+deployment_environment  = "dev"
+deployment_name         = "hello-world"
+credentials             = "fuchicorp-service-account.json
---
+++ deployments/terraform/main.tf
@@ -0,0 +1,36 @@
+module "helm_deploy" {
+  source                    = "fuchicorp/chart/helm"
+  deployment_name           = "${var.deployment_name}"
+  deployment_environment    = "${var.deployment_environment}"
+  deployment_endpoint       = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+  deployment_path           = "hello-world"
+  template_custom_vars = {
+    deployment_image        = "${var.deployment_image}"
+  }
+}
+
+variable "deployment_name" {
+    default = "hello-world"
+    description = "- (Optional) The name of the deployment."
+}
+variable "deployment_environment" {
+    default = "dev"
+    description = "- (Optional) The namespace to deploy the helm chart."
+}
+variable "deployment_endpoint" {
+    type = "map"
+    default = {
+        dev     = "dev.hello"
+        qa      = "qa.hello"
+        prod    = "hello"
+        stage   = "stage.hello"
+    }
+    description = "- (Optional) The endpooint of the deployment."
+}
+variable "google_domain_name" {
+  default = "kundaulet.com"
+}
+variable "deployment_image" {
+    default = "docker.saltadevops.com/hello-world-stage:97ad9d8"
+    description = "- (Optional) The docker image of the deployment."
+}
